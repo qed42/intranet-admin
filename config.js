5d27e53ddd7e4626f78ae7c9bf43ba7acf8e3be9
Pratik Bothra <pratik60@gmail.com>
2014-11-21 15:38:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 exports.config = [{
   'name': 'blue',
   'description': 'Blue and white',
-  'thumbnail': 'http://bootswatch.com/amelia/thumbnail.png',
+  'thumbnail': 'http://i.imgur.com/RxalYpn.jpg?1',
   'css': '/base/bootswatch_themes/blue/css/bootswatch_main.css',
   'common_css': 'base/bootswatch_themes/blue/blue.css'
 }, {
   'name': 'red',
   'description': 'Red and white',
-  'thumbnail': 'http://bootswatch.com/amelia/thumbnail.png',
+  'thumbnail': 'http://i.imgur.com/81Uzjni.jpg?1',
   'css': '/base/bootswatch_themes/red/css/bootswatch_main.css',
   'common_css': 'base/bootswatch_themes/red/red.css'
 }];
